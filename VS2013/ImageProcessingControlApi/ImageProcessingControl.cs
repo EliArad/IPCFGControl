b8a7cf1d57d8855ffc41f9a8d6f6716526a81c0c
easp13 <easp13@gmail.com>
2017-10-18 04:52:41 +0300

    update udp

--- VS2013/ImageProcessingControlApi/ImageProcessingControl.cs
+++ VS2013/ImageProcessingControlApi/ImageProcessingControl.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using CommonLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ImageProcessingControlApi
 {
     public class ImageProcessingControl
     {
+
+        [DllImport("ImageProcessingLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void IPStartProcess();
+
+        [DllImport("ImageProcessingLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int IPGetResult(out float result);
+
+        [DllImport("ImageProcessingLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void IPClose();
+
+        [DllImport("ImageProcessingLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void IPStop();
+
+        public AppCommon.APPErrors Start(AutoResetEvent ev)
+        {
+            IPStartProcess();
+            return AppCommon.APPErrors.STATUS_OK;
+        }
+
+        public AppCommon.APPErrors Stop()
+        {
+            return AppCommon.APPErrors.STATUS_OK;
+        }
     }
+  
 }
---
+++ VS2013/UdpServer/UDPServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpServer
+{
+    public class UDPServer
+    {
+
+        int m_port;
+
+        UdpClient m_newsock;
+
+        public UDPServer(int port = 9050)
+        {
+
+            m_port = port;
+        }
+     
+        public void Start()
+        {
+
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, m_port);
+            UdpClient m_newsock = new UdpClient(ipep);
+         
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+            data = m_newsock.Receive(ref sender);
+
+            Console.WriteLine("Message received from {0}:", sender.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+
+            string welcome = "Welcome to my test server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            m_newsock.Send(data, data.Length, sender);
+        }
+    }
+}
